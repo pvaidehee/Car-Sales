986a05618d1af301623153c6d399cbf3da3a012c
vaidehee patel <pvaidehee@gmail.com>
2020-09-09 00:14:47 +0530

    it's working finally

---
+++ src/actions/rootActions.js
@@ -0,0 +1,13 @@
+export const ADD_FEATURE = "ADD_FEATURE";
+
+export const addFeature = (feature) => {
+    return { type: ADD_FEATURE, 
+        payload: feature };
+};
+
+export const SUBTRACT_FEATURE = "SUBTRACT_FEATURE";
+
+export const subtractFeature = (feature) => {
+    return { typr: SUBTRACT_FEATURE,
+    payload: feature };
+};
